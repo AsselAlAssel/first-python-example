1ebf3b8d8678e8c9896b8de2764220ae8f1b27d8
Assel Al Assel <aseel1234500000@gmail.com>
2022-03-12 17:00:17 +0200

    some examples by py

---
+++ test1.py
@@ -0,0 +1,71 @@
+
+# from distutils.log import error
+# from doctest import debug
+
+
+# def oddOrEven(value):
+#     if isdevideBy2(value):
+#         return "Even"
+#     return "Odd"
+
+
+# def isdevideBy2(value):
+#     return value % 2 == 0
+
+
+# while True:
+#     print("enter any number positive for check if even or odd ,enter -1 for exit")
+#     try:
+#         enterdValue = int(input())
+#         if enterdValue == -1:
+#             break
+#         elif enterdValue < -1:
+#             raise Exception("Error")
+#         print(oddOrEven(enterdValue))
+
+#     except:
+#         debug
+#         print("u input a wrong value not number or minus")
+#         break
+# ***************************************************************
+
+def firstTriangle(rowNumber):
+    for row in reversed(range(rowNumber)):
+        print("*"*(row+1))
+    print("--------------------------------")
+
+
+def secondTriangle(rowNumber):
+
+    for row in range(rowNumber):
+        print("*"*(row+1))
+
+    print("--------------------------------")
+
+
+def thirdTriangle(rowNumber):
+
+    for row in reversed(range(rowNumber)):
+        print(" "*(rowNumber-(row+1)), end="")
+        print("*"*(row+1))
+    print("--------------------------------")
+
+def fourthTriangle(rowNumber):
+
+    for row in range(rowNumber):
+        print(" "*(rowNumber-(row+1)), end="")
+        print("*"*(row+1))
+    print("--------------------------------")
+
+print("enter any number from 1 to 9")
+try:
+    rowNumber = int(input())
+    if rowNumber > 0 and rowNumber < 10:
+        firstTriangle(rowNumber)
+        secondTriangle(rowNumber)
+        thirdTriangle(rowNumber)
+        fourthTriangle(rowNumber)
+    else:
+        print("u entered a wrong input not anumber")
+except:
+    print("u entered a wrong input not anumber")
